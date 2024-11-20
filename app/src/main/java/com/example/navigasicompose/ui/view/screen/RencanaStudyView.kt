73a038c576fce27faec2176754568182a10b9008
FirdausyTama <atama.firdausy@gmail.com>
2024-11-20 23:19:52 +0700

    membuat icon notification

--- app/src/main/java/com/example/navigasicompose/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigasicompose/ui/view/screen/RencanaStudyView.kt
@@ -10,6 +10,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -78,7 +81,9 @@ fun RencanaStudiView(
                 )
             }
             Box {
-
+                Icon(
+                    imageVector = Icons.Filled.Notifications,
+                )
             }
         }
     }
