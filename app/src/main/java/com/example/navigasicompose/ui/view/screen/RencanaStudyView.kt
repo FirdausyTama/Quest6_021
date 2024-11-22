3cb550081a501426e866ae668d4b0d4d440fb005
FirdausyTama <atama.firdausy@gmail.com>
2024-11-22 12:39:34 +0700

    menambah row

--- app/src/main/java/com/example/navigasicompose/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigasicompose/ui/view/screen/RencanaStudyView.kt
@@ -2,11 +2,13 @@ package com.example.navigasicompose.ui.view.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -133,6 +135,10 @@ fun RencanaStudiView(
                     fontWeight = FontWeight.Light
                 )
                 Spacer(modifier = Modifier.padding(8.dp))
+                Row (
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ){}
             }
         }
     }
