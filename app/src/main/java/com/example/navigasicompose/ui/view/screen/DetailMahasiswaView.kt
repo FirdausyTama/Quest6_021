b4908c6c4c4b5f41c6524e1427da3778493fd237
FirdausyTama <atama.firdausy@gmail.com>
2024-11-22 16:25:56 +0700

    finish

--- app/src/main/java/com/example/navigasicompose/ui/view/screen/DetailMahasiswaView.kt
+++ app/src/main/java/com/example/navigasicompose/ui/view/screen/DetailMahasiswaView.kt
@@ -26,6 +26,10 @@ fun DetailMahasiswaView(
         Pair("NIM", dataMhs.nim),
 
     )
+    val listDataKRS = listOf(
+        Pair("Mata Kuliah", DataKRS.mataKuliah),
+        Pair("Kelas", DataKRS.kelas)
+    )
 
     Column (
         modifier = Modifier
@@ -33,6 +37,9 @@ fun DetailMahasiswaView(
             .padding(16.dp)
     ){
         listDataMhs.forEach { data ->
+            DetailMhs(judul = data.first, isinya = data.second)
+        }
+        listDataKRS.forEach { data ->
             DetailMhs(judul = data.first, isinya = data.second)
         }
         Button(
