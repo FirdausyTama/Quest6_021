33e36412d3c55596c2a7b8399cb810d9b078ba7f
FirdausyTama <atama.firdausy@gmail.com>
2024-11-22 12:30:56 +0700

    membuat membuat fungsi savedatamahasiswa

--- app/src/main/java/com/example/navigasicompose/ui/view/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/navigasicompose/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -1,4 +1,20 @@
 package com.example.navigasicompose.ui.view.viewmodel
 
-class MahasiswaViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.navigasicompose.model.Mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class MahasiswaViewModel : ViewModel(){
+
+    private val _mahasiswaStateUI = MutableStateFlow(Mahasiswa())
+    val mahasiswaUIState: StateFlow<Mahasiswa> = _mahasiswaStateUI.asStateFlow()
+    fun saveDataMahasiswa(ls: MutableList<String>){
+        _mahasiswaStateUI.value = Mahasiswa(
+            nim = ls[0],
+            nama = ls[1],
+            email = ls[2]
+        )
+    }
 }
